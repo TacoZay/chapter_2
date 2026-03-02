edf2550b44f976b99799444f1df6c910cb1e9929
Xay <xjohnson399@gmail.com>
2026-03-02 00:36:05 -0500

    Starting exercise9 in chapter2

---
+++ src/exercise9.java
@@ -0,0 +1,20 @@
+/*Newton’s equation of motion for distance covered given initial
+velocity, constant acceleration, and total time is described by the expression
+s = ut + 1/2* ((at)^2) 
+
+Write a program that prompts the user to enter the initial velocity u in meters/
+second, time span t in seconds and acceleration a in meters/second2, and returns
+the distance covered. Here is a sample run
+    Enter u, t and a: 3.5 4 6.5
+    The distance covered is 14.0*/
+import java.util.Scanner;
+
+public class exercise9 {
+    public static void main(String args){
+        Scanner sc = new Scanner(System.in);
+        int u, t, a;
+        //How do i assign multiple variables for 
+        System.out.print("Enter u, t, and a: ");
+        u =sc.nextInt();
+    }
+}
