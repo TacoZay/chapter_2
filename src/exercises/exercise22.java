28c24d98d3a6739a0b1aa8e51120092baec40fc8
TacoZay <xjohnson399@gmail.com>
2026-04-24 21:02:24 -0400

    Implement exercise 22: Fix monetary unit conversion accuracy and add user input handling

--- src/exercises/exercise22.java
+++ src/exercises/exercise22.java
@@ -1,5 +1,30 @@
 package exercises;
 
+import java.util.Scanner;
+
+/*(Financial application: monetary units)
+Rewrite Listing 2.10, ComputeChange.java to fix the possible
+loss of accuracy when converting a double value to an int
+value. Enter the input as an integer whose last two digits
+represent the cents. For  */
 public class exercise22 {
-    
+    public static void main(String[] args){
+        //Create a Scanner
+        Scanner sc = new Scanner(System.in);
+
+        //Recieve the amount
+        System.out.print("Enter an amount in double, for example 11.56: ");
+        int amount = sc.nextInt();
+
+        int dollars = amount / 100;
+        // amount -= dollars;
+        // amount *= 100;
+
+        
+
+        System.out.println(dollars);
+        System.out.println(amount);
+
+
+    }
 }
